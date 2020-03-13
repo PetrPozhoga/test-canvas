53db38cfc25aa18ab881f86659e9e2ebceece028
PetrPozhoga <pet24489@gmail.com>
2020-03-13 20:35:44 +0200

    add circle movement functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import arcList from './resource/arcList'
 
 class CanvasMap {
-  constructor(dotList = [], circleRadius = 0) {
-    this.dotList = dotList
+
+  constructor(circleList = [], circleRadius = 0) {
+    this.circleList = circleList
     this.circleRadius = circleRadius
     this.start = {
       x: null,
       y: null,
-      left: null,
-      top: null,
-      setPosition(canvas) {
-        this.top = Number(getComputedStyle(canvas).top.replace('px', '')) || 0
-        this.left = Number(getComputedStyle(canvas).left.replace('px', '')) || 0
+      currentCircleIndex: -1,
+      currentElementX: null,
+      currentElementY: null,
+      setPosition(currentElement) {
+        this.currentElementY = currentElement && currentElement.y
+        this.currentElementX = currentElement && currentElement.x
       }
     }
   }
@@ -24,53 +26,72 @@ class CanvasMap {
     return { ctx, ctxWidth, ctxHeight, canvas }
   }
 
-  setDots() {
-    const { ctxWidth, ctxHeight, ctx } = this.getMap()
-    const pi = Math.PI
-    this.dotList.forEach(({ x, y }) => {
-      if ((x + this.circleRadius) > ctxWidth ||
+  filterCircles(list) {
+    const { ctxWidth, ctxHeight } = this.getMap()
+    return list.filter(({ x, y }) => {
+      return !(
+        (x + this.circleRadius) > ctxWidth ||
         (y + this.circleRadius) > ctxHeight ||
         y < this.circleRadius ||
-        x < this.circleRadius) return
-      else {
-        ctx.beginPath()
-        ctx.fillStyle = '#000'
-        ctx.arc(x, y, this.circleRadius, 0, pi * 2, false)
-        ctx.fill()
-      }
+        x < this.circleRadius
+      )
     })
   }
 
-  moveMap = ({ x, y }) => {
-    const { canvas } = this.getMap()
+  setCircles() {
+    const { ctx, ctxHeight, ctxWidth } = this.getMap()
+    ctx.clearRect(0, 0, ctxWidth, ctxHeight)
+    const pi = Math.PI
+    this.circleList = this.filterCircles(this.circleList)
+    this.circleList.forEach(({ x, y }) => {
+      ctx.beginPath()
+      ctx.fillStyle = '#000'
+      ctx.arc(x, y, this.circleRadius, 0, pi * 2, false)
+      ctx.fill()
+      ctx.closePath()
+    })
+  }
 
+  moveCircle = ({ x, y }) => {
     if (!this.start.x || !this.start.y) {
       this.start.x = x
       this.start.y = y
-      if (!this.start.top || !this.start.left) this.start.setPosition(canvas)
+      if (!this.start.currentElementY || !this.start.currentElementX) this.start.setPosition(this.circleList[ this.currentCircleIndex ])
     }
-
-    canvas.style.left = x - this.start.x + this.start.left + 'px'
-    canvas.style.top = y - this.start.y + this.start.top + 'px'
+    x = x - this.start.x + this.start.currentElementX
+    y = y - this.start.y + this.start.currentElementY
+    if (this.filterCircles([ { x, y } ]).length) {
+      this.circleList[ this.currentCircleIndex ] = { x, y }
+      this.setCircles()
+    }
   }
 
   setDefaultStart = () => {
     this.start.x = null
     this.start.y = null
-    this.start.setPosition(canvas)
-    document.body.removeEventListener('mousemove', this.moveMap)
+    this.start.setPosition()
+    canvas.removeEventListener('mousemove', this.moveCircle)
     document.body.removeEventListener('mouseup', this.setDefaultStart)
   }
 
-  onMouseDown = () => {
-    document.body.addEventListener('mousemove', this.moveMap)
-    document.body.addEventListener('mouseup', this.setDefaultStart)
+  cursorInCircle = (x, y) => this.circleList.findIndex((item) => {
+    return (item.x + this.circleRadius >= x && item.x - this.circleRadius <= x) &&
+      (item.y + this.circleRadius >= y && item.y - this.circleRadius <= y)
+  })
+
+  onMouseDown = ({ currentTarget, x, y }) => {
+    this.currentCircleIndex = this.cursorInCircle(x, y)
+
+    if (this.currentCircleIndex >= 0) {
+      currentTarget.addEventListener('mousemove', this.moveCircle)
+      document.body.addEventListener('mouseup', this.setDefaultStart)
+    }
   }
 }
 
 const simpleCanvasMap = new CanvasMap(arcList, 5)
 
-simpleCanvasMap.setDots()
+simpleCanvasMap.setCircles()
 
 const { canvas } = simpleCanvasMap.getMap()
 
